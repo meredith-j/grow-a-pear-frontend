8b278e1c9d8fd7bd4d6c7aeb0d362cd49d40fdce
Meredith Jonatan <mjonatan@me.com>
2023-08-07 18:14:51 -0400

    add garden quiz page & link where needed

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Page() {
     <div className={styles.info}>
       <p className={styles.info_description}>Grow a Pear is currently in its Beta version -- we are crowdsourcing information on plants that you have grown. We are working hard on releasing a full version soon, but in the meantime, please take our survey!</p>
       <Link
-        a href="/"
+        a href="/your-garden"
         className={styles.info_start}>
           <p className={styles.info_link}>Get Started</p>
       </Link>
---
+++ src/app/your-garden/page.js
@@ -0,0 +1,7 @@
+import styles from '../styles/your-garden.module.scss'
+
+export default function Page() {
+    return (
+    <h1>Hello, World</h1>
+    )
+  }
