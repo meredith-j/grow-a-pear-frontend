1a0c99db975378c2e45c58a26163caa8c57988b9
Meredith Jonatan <mjonatan@me.com>
2023-09-04 02:37:19 -0400

    complete form validation and initial styles

--- src/app/components/form.js
+++ src/app/components/form.js
@@ -11,6 +11,7 @@ export default function Form() {
     const [plant, setPlant] = useState("")
     const [vibe, setVibe] = useState("")
     const [review, setReview] = useState("")
+    const [isValid, setIsValid] = useState(true)
 
     // add form fields
     const handleClick = () => {
@@ -23,46 +24,57 @@ export default function Form() {
         e.preventDefault();
 
         // form validation
+        // let errors = {};
+
         if (!city || city === "") {
-            console.log("City is required.")
+            // errors.city = "City is required."
+            setIsValid(false)
+            return
         }
 
         if (!province || province === "") {
-            console.log("Province is required.")
+            // errors.city = "City is required."
+            setIsValid(false)
+            return
         }
 
         if (!plant || plant === "") {
-            console.log("Plant is required.")
+            // errors.plant = "Plant is required."
+            setIsValid(false)
+            return
         }
 
         if (!vibe || vibe === "") {
-            console.log("Vibe is required.")
+            // errors.vibe = "Vibe is required."
+            setIsValid(false)
+            return
         }
 
-        else {
-            // create body for request
-            const newPlant = {};
-            newPlant.city = e.target.city.value;
-            newPlant.province = e.target.province.value;
-            newPlant.plant = e.target.plant.value;
-            newPlant.vibe = e.target.vibe.value;
-            newPlant.review = e.target.review.value;
+        // setErrors(errors)
 
-            console.log(newPlant)
+        // create body for request
+        const newPlant = {};
+        newPlant.city = e.target.city.value;
+        newPlant.province = e.target.province.value;
+        newPlant.plant = e.target.plant.value;
+        newPlant.vibe = e.target.vibe.value;
+        newPlant.review = e.target.review.value;
 
-            // send POST request
-            axios
-                .post(`http://localhost:8080/plant`, newPlant)
-                .then(() => {
+        console.log(newPlant)
+
+        // send POST request
+        axios
+            .post(`http://localhost:8080/plant`, newPlant)
+            .then(() => {
                     
-                    // navigate to thank you page (maybe email sign ups?)
+                // navigate to thank you page (maybe email sign ups?)
 
-                    console.log("🤘🏻")
-                })
-                .catch((err) => {
-                console.log(err);
-                });
-        }
+                console.log("🤘🏻")
+            })
+            .catch((err) => {
+            console.log(err);
+            });
+        
     }
 
     return (
@@ -73,57 +85,71 @@ export default function Form() {
                 <div className={styles.form_question}>
                     <label className={styles.form_label}>
                         Where do you live?</label>
-                    <input className={styles.form_location}
-                        type="text"
-                        name="city"
-                        placeholder="Your City"
-                        value={city}
-                        onChange={(e) => {
-                            setCity(e.target.value)}}
-                    />
-                    <input className={styles.form_location}
-                        type="text"
-                        name="province"
-                        placeholder="Your Province/Territory"
-                        value={province}
-                        onChange={(e) => {
-                            setProvince(e.target.value)}}
-                    />
+                    <div className={styles.form_validation}>
+                        <p className={`${styles.form_no_error} ${!isValid && city === "" ? styles.form_error : ""}`}>!</p>
+                        <input className={styles.form_location}
+                            type="text"
+                            name="city"
+                            placeholder="Your City"
+                            value={city}
+                            onChange={(e) => {
+                                setCity(e.target.value)}}
+                        />
+                    </div>
+                    <div className={styles.form_validation}>
+                        <p className={`${styles.form_no_error} ${!isValid && province === "" ? styles.form_error : ""}`}>!</p>
+                        <input className={styles.form_location}
+                            type="text"
+                            name="province"
+                            placeholder="Your Province/Territory"
+                            value={province}
+                            onChange={(e) => {
+                                setProvince(e.target.value)}}
+                        />
+                    </div>
                 </div>
                 <div className={styles.form_question}>
                     <label className={styles.form_label}>
                         What did you grow?</label>
-                    <input className={styles.form_plant}
-                        type="text"
-                        name="plant"
-                        placeholder="Your Plant"
-                        value={plant}
-                        onChange={(e) => {
-                            setPlant(e.target.value)}}
-                    />
+                    <div className={styles.form_validation}>
+                        <p className={`${styles.form_no_error} ${!isValid && plant === "" ? styles.form_error : ""}`}>!</p>
+                        <input className={styles.form_plant}
+                            type="text"
+                            name="plant"
+                            placeholder="Your Plant"
+                            value={plant}
+                            onChange={(e) => {
+                                setPlant(e.target.value)}}
+                        />
+                    </div>
                 </div>
                 <div className={styles.form_question}>
                     <label className={styles.form_label}>
                         Did you vibe with this plant?</label>
-                    <div className={styles.form_vibe}>
-                        <input className="vibe__option"
-                            type="radio" 
-                            name="vibe"
-                            value="false"
-                            onChange={(e) => {
-                            setVibe(e.target.value)}}
-                        />
-                        <p className={styles.form_vibe_label}>We did not vibe. 🥀</p>
-                    </div>
-                    <div className={styles.form_vibe}>
-                        <input className="vibe__option"
-                            type="radio" 
-                            name="vibe"
-                            value="true"
-                            onChange={(e) => {
-                            setVibe(e.target.value)}}
-                        />
-                        <p className={styles.form_vibe_label}>Hell yeah, we vibed 🤘🏻</p>
+                    <div className={styles.form_vibes_validation}>
+                        <p className={`${styles.form_no_error} ${!isValid && vibe === "" ? styles.form_error : ""}`}>!</p>
+                        <div className={styles.form_vibes}>
+                            <div className={styles.form_vibe}>
+                                <input className={styles.form_vibe_option}
+                                    type="radio" 
+                                    name="vibe"
+                                    value="false"
+                                    onChange={(e) => {
+                                    setVibe(e.target.value)}}
+                                />
+                                <p className={styles.form_vibe_label}>We did not vibe. 🥀</p>
+                            </div>
+                            <div className={styles.form_vibe}>
+                                <input className={styles.form_vibe_option}
+                                    type="radio" 
+                                    name="vibe"
+                                    value="true"
+                                    onChange={(e) => {
+                                    setVibe(e.target.value)}}
+                                />
+                                <p className={styles.form_vibe_label}>Hell yeah, we vibed 🤘🏻</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className={styles.form_question}>
