b5d36a0d0f297e970c28380d3cc42cd7c41d9524
Meredith Jonatan <mjonatan@me.com>
2024-04-07 22:32:13 -0400

    added test file for thank you page, tests all passing

---
+++ __tests__/thankyou.test.js
@@ -0,0 +1,38 @@
+import Page from "../src/app/thank-you/page";
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe('thank you page', () => {
+
+  // this test should always pass: if it fails, something is wrong.
+  test('renders a heading', () => {
+
+    render(<Page />);
+
+    const heading = screen.getByRole('heading', {
+        name: /Thanks for submitting 💖/i,
+    })
+
+        expect(heading).toBeInTheDocument();
+        expect(heading).toHaveTextContent('Thanks for submitting 💖');
+
+  })
+
+  // link that goes back to garden quiz works
+  test('link to quiz working', () => {
+    render(<Page />);
+
+    const link = screen.getByText("Submit more plants");
+
+    expect(link.getAttribute("href")).toBe("/your-garden");
+  })
+
+  // link that goes back to home works
+  test('link to home page working', () => {
+    render(<Page />);
+
+    const link = screen.getByText("Home");
+
+    expect(link.getAttribute("href")).toBe("/");
+  })
+})
