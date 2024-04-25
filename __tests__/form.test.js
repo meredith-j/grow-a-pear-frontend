fdc15f94f28413306db9ce1b2f5c48eae063019c
Meredith Jonatan <mjonatan@me.com>
2024-04-25 17:14:10 -0400

    clean up form unit test file

--- __tests__/form.test.js
+++ __tests__/form.test.js
@@ -11,17 +11,6 @@ jest.mock('next/navigation', () => ({
   
 describe('plant quiz/form', () => {
 
-    // before each (render Page)
-    // before each/after each (look into syntax)
-
-    // descibe -- validation
-
-    // describe -- ui
-
-    // describe -- repeater
-    
-    // this test should always pass: if it fails, something is wrong.
-
     describe('tests that everything on page is working', () => {
         test('renders a header', () => {
             render(<Page />);
@@ -53,75 +42,5 @@ describe('plant quiz/form', () => {
             const element = screen.getByRole('button', { name: 'Save my plants' });
             expect(element).toBeInTheDocument();
         });
-
-        // alt test submit button
-        // test("NEW plants are submitted when submit button is clicked", () => {
-        //     const {getByTestId} = render(<Page 
-        //         handleSubmit={submit}/>);
-
-        //     const submit = getByTestId('submit-test');
-        //     fireEvent.click(submit);
-        //     expect(submit).toHaveBeenCalled()
-        // })
-
-        // OLD test submit button
-        
-        test("plants with correct information are submitted and sent to database", () => {
-            const mockHandleSubmit = jest.fn(console.log('hello'));
-            const mockHandleSubmit1 = jest.fn(console.log('hello 2'));
-            
-            // const myMockFn = jest.fn(cb => cb(null, true));
-            // myMockFn((err, val) => console.log(val));
-
-            const {getByText} = render(<Page
-                email={""}
-                city={"toronto"}
-                province={'ontario'}
-                isValid={true}
-                plantData={[{plant:"tomato", vibe: false, review: ""}]}
-                handleAddPlant={Function}
-                handleRemovePlant={Function}
-                handleOnSubmit={mockHandleSubmit} 
-                />);
-            
-            const button = getByText("Save my plants");
-    
-            userEvent.click(button);
-            
-            expect(button).toBeInTheDocument()
-            expect(mockHandleSubmit).toHaveBeenCalled();
-            expect(mockHandleSubmit1).toHaveBeenCalled();
-
-        })
-
-        // "add plant" button adds a plant
-        // test('new plant input', () => {
-        //     const mockHandleAddPlant = jest.fn();
-
-        //     // "testing library fireevent click tohavebeencalled"
-
-        //     const {getByText} = render(<Page
-        //         // onClick={mockHandleAddPlant}
-        //         // email={""}
-        //         // city={""}
-        //         // province={''}
-        //         // isValid={true}
-        //         // plantData={[{plant:"", vibe: "", review: ""}]}
-        //         // handleAddPlant={mockHandleAddPlant}
-        //         // handleRemovePlant={Function}
-        //         // handleSubmit={Function} 
-        //         />);
-
-        //     const button = getByText("Add another plant");
-                
-        //     // jest.spyOn().mockImplementation(); ???
-
-        //     // fireEvent(myElement, new MouseEvent('click', { bubbles: true }));
-
-        //     // fireEvent(button, new MouseEvent('click', {bubbles: true}));
-        //         fireEvent.click(button);
-                
-        //         expect(mockHandleAddPlant).toHaveBeenCalled();
-        // })
     })
 })
---
+++ __tests__/formValidation.test.js
@@ -0,0 +1,10 @@
+    // before each (render Page)
+    // before each/after each (look into syntax)
+
+    // descibe -- validation
+
+    // describe -- ui
+
+    // describe -- repeater
+    
+    // this test should always pass: if it fails, something is wrong.
