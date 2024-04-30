fafda2ef182f52de7c7f2bc84a6f400de3c3a21a
Meredith Jonatan <mjonatan@me.com>
2024-04-29 23:38:34 -0400

    completed tests on form validation

--- __tests__/formValidation.test.js
+++ __tests__/formValidation.test.js
@@ -5,19 +5,99 @@ import { render, screen } from "@testing-library/react";
 import { formValidation } from "@/src/app/functions/formValidation";
 
 describe('form validation function', () => {
-    // describe('all of these tests should return FALSE', () =>{
-        
-    // });
+    describe('all of these tests should return FALSE', () =>{
+        test('one plant, city & NO province', async () => {
+            const city = "toronto";
+            const province = "";
+            const plantData = [{plant: "tomato", vibe: true, review: ""}];
 
-    describe('all of these tests should return TRUE', () =>{
-        test('one plant, city & province', () => {
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        });
+        test('one plant, province & NO city', async () => {
+            const city = "";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: true, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        }); 
+        test('one plant, city & province, NO plant name', async () => {
             const city = "toronto";
             const province = "ontario";
-            const plantData = [{plant: "tomato", vibe: "true", review: ""}];
+            const plantData = [{plant: "", vibe: true, review: ""}];
 
-            const mockFormValidation = formValidation(city, province, plantData)
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        }); 
+        test('one plant, city & province, NO plant vibe', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: undefined, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        });
+        test('two plants, city & province, NO plant vibe (one plant)', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: undefined, review: ""}, {plant: "tomato", vibe: false, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        });
+        test('two plants, city & province, NO plant vibe (one plant)', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: true, review: ""}, {plant: "tomato", vibe: undefined, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        }); 
+        test('five plants, city & province, NO plant vibe (one plant)', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: true, review: ""}, {plant: "tomato", vibe: true, review: ""}, {plant: "tomato", vibe: undefined, review: ""}, {plant: "tomato", vibe: false, review: ""}, {plant: "tomato", vibe: true, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeFalsy();
+        });   
+    });
+
+    describe('all of these tests should return TRUE', () =>{
+        test('one plant, city & province', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: true, review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+
+            expect(mockFormValidation).toBeTruthy();
+        });
+        test('two plants, city & province', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: "true", review: ""}, {plant: "tomato", vibe: "true", review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
             
             expect(mockFormValidation).toBeTruthy();
-        })
+        });
+        test('five plants, city & province', async () => {
+            const city = "toronto";
+            const province = "ontario";
+            const plantData = [{plant: "tomato", vibe: "true", review: ""}, {plant: "tomato", vibe: "true", review: ""}, {plant: "tomato", vibe: "true", review: ""}, {plant: "tomato", vibe: "true", review: ""}, {plant: "tomato", vibe: "true", review: ""}];
+
+            const mockFormValidation = await formValidation(city, province, plantData)
+            
+            expect(mockFormValidation).toBeTruthy();
+        });
     })
 })
--- src/app/functions/formValidation.js
+++ src/app/functions/formValidation.js
@@ -1,25 +1,27 @@
 async function formValidation (city, province, plantData) {
 
-    console.log("validation function starts")
+    // console.log("validation function starts")
 
-    let validation;
+    let validation = true;
 
     if (!city || !province) {
         validation = false;
-        console.log("false city/province")
+        // console.log("false city/province")
+        // console.log(validation)
         return validation
     }
 
     for (let i = 0; i < plantData.length; i++) {
         if (plantData[i].plant === "" || plantData[i].vibe === undefined) {
             validation = false;
-            console.log("false plant data")
+            // console.log("false plant data")
             return validation
         }
 
     }
 
-    console.log("all true, wahoo!")
+    // console.log("all true, wahoo!")
+    return validation
 }
 
 export {formValidation}
