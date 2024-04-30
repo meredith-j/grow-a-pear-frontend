fddc23d43b679e5d3857aa2e379a36ead4a5481d
Meredith Jonatan <mjonatan@me.com>
2024-04-29 23:39:41 -0400

    remove console logs from form validation function

--- src/app/functions/formValidation.js
+++ src/app/functions/formValidation.js
@@ -1,26 +1,20 @@
 async function formValidation (city, province, plantData) {
 
-    // console.log("validation function starts")
-
     let validation = true;
 
     if (!city || !province) {
         validation = false;
-        // console.log("false city/province")
-        // console.log(validation)
         return validation
     }
 
     for (let i = 0; i < plantData.length; i++) {
         if (plantData[i].plant === "" || plantData[i].vibe === undefined) {
             validation = false;
-            // console.log("false plant data")
             return validation
         }
 
     }
 
-    // console.log("all true, wahoo!")
     return validation
 }
 
